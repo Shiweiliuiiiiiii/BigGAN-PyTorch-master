66e74b8db5f342065a8097950329afdc49f91b4a
刘世伟 <liushiwei@v099234.vpn.tue.nl>
2022-10-04 11:22:42 -0500

    add snip gmp

--- scripts/sample_cifar_ema_GMP_SNIP.sh
+++ scripts/sample_cifar_ema_GMP_SNIP.sh
@@ -14,7 +14,7 @@ conda activate slak
 python sample.py \
 --shuffle --batch_size 50 --G_batch_size 256 --parallel \
 --sparse --density 0.3 --G_growth gradient --D_growth random \
---experiment_name weights/sparse_fix_sema_imbalanced_densityD_0.3000_densityG_0.0500_dy_G_D_growth_random_G_growth_gradient_fre_5000_BigGAN_C10_seed1_Gch64_Dch64_bs50_nDs4_Glr2.0e-04_Dlr2.0e-04_Gnlrelu_Dnlrelu_GinitN02_DinitN02_ema \
+--experiment_name sparse_fix_sema_imbalanced_densityD_0.3000_densityG_0.0500_dy_G_D_growth_random_G_growth_gradient_fre_5000_BigGAN_C10_seed1_Gch64_Dch64_bs50_nDs4_Glr2.0e-04_Dlr2.0e-04_Gnlrelu_Dnlrelu_GinitN02_DinitN02_ema \
 --num_G_accumulations 1 --num_D_accumulations 1 --num_epochs 500 \
 --num_D_steps 4 --G_lr 2e-4 --D_lr 2e-4 \
 --dataset C10 \
@@ -23,12 +23,12 @@ python sample.py \
 --G_init N02 --D_init N02 \
 --ema --use_ema --ema_start 1000 \
 --test_every 5000 --save_every 2000 --num_best_copies 5 --num_save_copies 2 --seed 0 --sample_npz --load_weights copy0
-python inception_tf13.py --experiment_name weights/sparse_fix_sema_imbalanced_densityD_0.3000_densityG_0.0500_dy_G_D_growth_random_G_growth_gradient_fre_5000_BigGAN_C10_seed1_Gch64_Dch64_bs50_nDs4_Glr2.0e-04_Dlr2.0e-04_Gnlrelu_Dnlrelu_GinitN02_DinitN02_ema
+python inception_tf13.py --experiment_name sparse_fix_sema_imbalanced_densityD_0.3000_densityG_0.0500_dy_G_D_growth_random_G_growth_gradient_fre_5000_BigGAN_C10_seed1_Gch64_Dch64_bs50_nDs4_Glr2.0e-04_Dlr2.0e-04_Gnlrelu_Dnlrelu_GinitN02_DinitN02_ema
 
 python sample.py \
 --shuffle --batch_size 50 --G_batch_size 256 --parallel \
 --sparse --density 0.3 --G_growth gradient --D_growth random \
---experiment_name weights/sparse_fix_sema_imbalanced_densityD_0.3000_densityG_0.1000_dy_G_D_growth_random_G_growth_gradient_fre_5000_BigGAN_C10_seed1_Gch64_Dch64_bs50_nDs4_Glr2.0e-04_Dlr2.0e-04_Gnlrelu_Dnlrelu_GinitN02_DinitN02_ema \
+--experiment_name sparse_fix_sema_imbalanced_densityD_0.3000_densityG_0.1000_dy_G_D_growth_random_G_growth_gradient_fre_5000_BigGAN_C10_seed1_Gch64_Dch64_bs50_nDs4_Glr2.0e-04_Dlr2.0e-04_Gnlrelu_Dnlrelu_GinitN02_DinitN02_ema \
 --num_G_accumulations 1 --num_D_accumulations 1 --num_epochs 500 \
 --num_D_steps 4 --G_lr 2e-4 --D_lr 2e-4 \
 --dataset C10 \
@@ -37,12 +37,12 @@ python sample.py \
 --G_init N02 --D_init N02 \
 --ema --use_ema --ema_start 1000 \
 --test_every 5000 --save_every 2000 --num_best_copies 5 --num_save_copies 2 --seed 0 --sample_npz --load_weights copy0
-python inception_tf13.py --experiment_name weights/sparse_fix_sema_imbalanced_densityD_0.3000_densityG_0.1000_dy_G_D_growth_random_G_growth_gradient_fre_5000_BigGAN_C10_seed1_Gch64_Dch64_bs50_nDs4_Glr2.0e-04_Dlr2.0e-04_Gnlrelu_Dnlrelu_GinitN02_DinitN02_ema
+python inception_tf13.py --experiment_name sparse_fix_sema_imbalanced_densityD_0.3000_densityG_0.1000_dy_G_D_growth_random_G_growth_gradient_fre_5000_BigGAN_C10_seed1_Gch64_Dch64_bs50_nDs4_Glr2.0e-04_Dlr2.0e-04_Gnlrelu_Dnlrelu_GinitN02_DinitN02_ema
 
 python sample.py \
 --shuffle --batch_size 50 --G_batch_size 256 --parallel \
 --sparse --density 0.3 --G_growth gradient --D_growth random \
---experiment_name weights/sparse_fix_sema_imbalanced_densityD_0.3000_densityG_0.2000_dy_G_D_growth_random_G_growth_gradient_fre_5000_BigGAN_C10_seed1_Gch64_Dch64_bs50_nDs4_Glr2.0e-04_Dlr2.0e-04_Gnlrelu_Dnlrelu_GinitN02_DinitN02_ema \
+--experiment_name sparse_fix_sema_imbalanced_densityD_0.3000_densityG_0.2000_dy_G_D_growth_random_G_growth_gradient_fre_5000_BigGAN_C10_seed1_Gch64_Dch64_bs50_nDs4_Glr2.0e-04_Dlr2.0e-04_Gnlrelu_Dnlrelu_GinitN02_DinitN02_ema \
 --num_G_accumulations 1 --num_D_accumulations 1 --num_epochs 500 \
 --num_D_steps 4 --G_lr 2e-4 --D_lr 2e-4 \
 --dataset C10 \
@@ -51,28 +51,13 @@ python sample.py \
 --G_init N02 --D_init N02 \
 --ema --use_ema --ema_start 1000 \
 --test_every 5000 --save_every 2000 --num_best_copies 5 --num_save_copies 2 --seed 0 --sample_npz --load_weights copy0
-python inception_tf13.py --experiment_name weights/sparse_fix_sema_imbalanced_densityD_0.3000_densityG_0.2000_dy_G_D_growth_random_G_growth_gradient_fre_5000_BigGAN_C10_seed1_Gch64_Dch64_bs50_nDs4_Glr2.0e-04_Dlr2.0e-04_Gnlrelu_Dnlrelu_GinitN02_DinitN02_ema
-
-
-python sample.py \
---shuffle --batch_size 50 --G_batch_size 256 --parallel \
---sparse --density 0.3 --G_growth gradient --D_growth random \
---experiment_name weights/sparse_GMP_sema_imbalanced_densityD_1.0000_densityG_0.0500_dy_G_D_growth_random_G_growth_gradient_fre_5000_BigGAN_C10_seed1_Gch64_Dch64_bs50_nDs4_Glr2.0e-04_Dlr2.0e-04_Gnlrelu_Dnlrelu_GinitN02_DinitN02_ema \
---num_G_accumulations 1 --num_D_accumulations 1 --num_epochs 500 \
---num_D_steps 4 --G_lr 2e-4 --D_lr 2e-4 \
---dataset C10 \
---G_ortho 0.0 \
---G_attn 0 --D_attn 0 \
---G_init N02 --D_init N02 \
---ema --use_ema --ema_start 1000 \
---test_every 5000 --save_every 2000 --num_best_copies 5 --num_save_copies 2 --seed 0 --sample_npz --load_weights copy0
-python inception_tf13.py --experiment_name weights/sparse_GMP_sema_imbalanced_densityD_1.0000_densityG_0.0500_dy_G_D_growth_random_G_growth_gradient_fre_5000_BigGAN_C10_seed1_Gch64_Dch64_bs50_nDs4_Glr2.0e-04_Dlr2.0e-04_Gnlrelu_Dnlrelu_GinitN02_DinitN02_ema
+python inception_tf13.py --experiment_name sparse_fix_sema_imbalanced_densityD_0.3000_densityG_0.2000_dy_G_D_growth_random_G_growth_gradient_fre_5000_BigGAN_C10_seed1_Gch64_Dch64_bs50_nDs4_Glr2.0e-04_Dlr2.0e-04_Gnlrelu_Dnlrelu_GinitN02_DinitN02_ema
 
 
 python sample.py \
 --shuffle --batch_size 50 --G_batch_size 256 --parallel \
 --sparse --density 0.3 --G_growth gradient --D_growth random \
---experiment_name weights/sparse_GMP_sema_imbalanced_densityD_1.0000_densityG_0.1000_dy_G_D_growth_random_G_growth_gradient_fre_5000_BigGAN_C10_seed1_Gch64_Dch64_bs50_nDs4_Glr2.0e-04_Dlr2.0e-04_Gnlrelu_Dnlrelu_GinitN02_DinitN02_ema \
+--experiment_name sparse_GMP_sema_imbalanced_densityD_1.0000_densityG_0.0500_dy_G_D_growth_random_G_growth_gradient_fre_5000_BigGAN_C10_seed1_Gch64_Dch64_bs50_nDs4_Glr2.0e-04_Dlr2.0e-04_Gnlrelu_Dnlrelu_GinitN02_DinitN02_ema \
 --num_G_accumulations 1 --num_D_accumulations 1 --num_epochs 500 \
 --num_D_steps 4 --G_lr 2e-4 --D_lr 2e-4 \
 --dataset C10 \
@@ -81,12 +66,13 @@ python sample.py \
 --G_init N02 --D_init N02 \
 --ema --use_ema --ema_start 1000 \
 --test_every 5000 --save_every 2000 --num_best_copies 5 --num_save_copies 2 --seed 0 --sample_npz --load_weights copy0
-python inception_tf13.py --experiment_name weights/sparse_GMP_sema_imbalanced_densityD_1.0000_densityG_0.1000_dy_G_D_growth_random_G_growth_gradient_fre_5000_BigGAN_C10_seed1_Gch64_Dch64_bs50_nDs4_Glr2.0e-04_Dlr2.0e-04_Gnlrelu_Dnlrelu_GinitN02_DinitN02_ema
+python inception_tf13.py --experiment_name sparse_GMP_sema_imbalanced_densityD_1.0000_densityG_0.0500_dy_G_D_growth_random_G_growth_gradient_fre_5000_BigGAN_C10_seed1_Gch64_Dch64_bs50_nDs4_Glr2.0e-04_Dlr2.0e-04_Gnlrelu_Dnlrelu_GinitN02_DinitN02_ema
+
 
 python sample.py \
 --shuffle --batch_size 50 --G_batch_size 256 --parallel \
 --sparse --density 0.3 --G_growth gradient --D_growth random \
---experiment_name weights/sparse_GMP_sema_imbalanced_densityD_1.0000_densityG_0.2000_dy_G_D_growth_random_G_growth_gradient_fre_5000_BigGAN_C10_seed1_Gch64_Dch64_bs50_nDs4_Glr2.0e-04_Dlr2.0e-04_Gnlrelu_Dnlrelu_GinitN02_DinitN02_ema \
+--experiment_name sparse_GMP_sema_imbalanced_densityD_1.0000_densityG_0.1000_dy_G_D_growth_random_G_growth_gradient_fre_5000_BigGAN_C10_seed1_Gch64_Dch64_bs50_nDs4_Glr2.0e-04_Dlr2.0e-04_Gnlrelu_Dnlrelu_GinitN02_DinitN02_ema \
 --num_G_accumulations 1 --num_D_accumulations 1 --num_epochs 500 \
 --num_D_steps 4 --G_lr 2e-4 --D_lr 2e-4 \
 --dataset C10 \
@@ -95,4 +81,18 @@ python sample.py \
 --G_init N02 --D_init N02 \
 --ema --use_ema --ema_start 1000 \
 --test_every 5000 --save_every 2000 --num_best_copies 5 --num_save_copies 2 --seed 0 --sample_npz --load_weights copy0
-python inception_tf13.py --experiment_name weights/sparse_GMP_sema_imbalanced_densityD_1.0000_densityG_0.2000_dy_G_D_growth_random_G_growth_gradient_fre_5000_BigGAN_C10_seed1_Gch64_Dch64_bs50_nDs4_Glr2.0e-04_Dlr2.0e-04_Gnlrelu_Dnlrelu_GinitN02_DinitN02_ema
+python inception_tf13.py --experiment_name sparse_GMP_sema_imbalanced_densityD_1.0000_densityG_0.1000_dy_G_D_growth_random_G_growth_gradient_fre_5000_BigGAN_C10_seed1_Gch64_Dch64_bs50_nDs4_Glr2.0e-04_Dlr2.0e-04_Gnlrelu_Dnlrelu_GinitN02_DinitN02_ema
+
+python sample.py \
+--shuffle --batch_size 50 --G_batch_size 256 --parallel \
+--sparse --density 0.3 --G_growth gradient --D_growth random \
+--experiment_name sparse_GMP_sema_imbalanced_densityD_1.0000_densityG_0.2000_dy_G_D_growth_random_G_growth_gradient_fre_5000_BigGAN_C10_seed1_Gch64_Dch64_bs50_nDs4_Glr2.0e-04_Dlr2.0e-04_Gnlrelu_Dnlrelu_GinitN02_DinitN02_ema \
+--num_G_accumulations 1 --num_D_accumulations 1 --num_epochs 500 \
+--num_D_steps 4 --G_lr 2e-4 --D_lr 2e-4 \
+--dataset C10 \
+--G_ortho 0.0 \
+--G_attn 0 --D_attn 0 \
+--G_init N02 --D_init N02 \
+--ema --use_ema --ema_start 1000 \
+--test_every 5000 --save_every 2000 --num_best_copies 5 --num_save_copies 2 --seed 0 --sample_npz --load_weights copy0
+python inception_tf13.py --experiment_name sparse_GMP_sema_imbalanced_densityD_1.0000_densityG_0.2000_dy_G_D_growth_random_G_growth_gradient_fre_5000_BigGAN_C10_seed1_Gch64_Dch64_bs50_nDs4_Glr2.0e-04_Dlr2.0e-04_Gnlrelu_Dnlrelu_GinitN02_DinitN02_ema
